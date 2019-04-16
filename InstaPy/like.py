74356f460f35ea00fc7f1f347239bb053d3bd6a3
EC2 Default User <ec2-user@ip-172-31-30-177.us-east-2.compute.internal>
2019-04-16 12:56:58 +0000

    re post

---
+++ InstaPy/like.py
@@ -0,0 +1,21 @@
+""" Quickstart script for InstaPy usage """
+
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+import sys
+args = sys.argv
+#akuseuihf
+#ryumizii
+
+# set workspace folder at desired location (default is at your home folder)
+set_workspace(path=None)
+
+# get an InstaPy session!
+session = InstaPy(username=args[1], password=args[2], headless_browser=True)
+
+with smart_run(session):
+    """ Activity flow """
+    # general settings
+    session.follow_by_tags(['handmade', 'ハンドメイド'], amount=10)
